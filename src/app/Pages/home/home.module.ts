3c2f4cdbae5c58481e32c7f3d7f7ed4592114581
Fillipe16 <fillpe2510@hotmail.com>
2024-10-28 11:28:48 -0300

    Organizando pastas dos componentes

--- src/app/Pages/home/home.module.ts
+++ src/app/Pages/home/home.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../header/header.component';
-import { PostsContentComponent } from '../posts-content/posts-content.component';
-import { FooterComponent } from '../footer/footer.component';
-import { RecentContentComponent } from '../recent-content/recent-content.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { PostsContentComponent } from '../../components/posts-content/posts-content.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { RecentContentComponent } from '../../components/recent-content/recent-content.component';
 import { HomeComponent } from './home.component';
-import { MenuItemComponent } from '../header/menu-item/menu-item.component';
-import { RecentBigCardComponent } from '../recent-content/recent-big-card/recent-big-card.component';
-import { RecentCardComponent } from '../recent-content/recent-card/recent-card.component';
+import { MenuItemComponent } from '../../components/header/menu-item/menu-item.component';
+import { RecentBigCardComponent } from '../../components/recent-content/recent-big-card/recent-big-card.component';
+import { RecentCardComponent } from '../../components/recent-content/recent-card/recent-card.component';
 
 
 
